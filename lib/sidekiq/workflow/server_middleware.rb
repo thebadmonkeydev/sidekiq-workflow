16512e26a48a38fee093f42cb27c0c48f5e61f5d
Michael Kelly <michaelkelly322@gmail.com>
2015-06-10 16:23:59 -0400

    Removes redis_pool from server middleware

--- lib/sidekiq/workflow/server_middleware.rb
+++ lib/sidekiq/workflow/server_middleware.rb
@@ -1,6 +1,6 @@
 class Sidekiq::Workflow::ServerMiddleware
-  def call(worker_class, msg, queue, redis_pool)
-    puts "Sidekiq::Workflow::ServerMiddleware called with:\n  worker_class: #{worker_class}\n  msg: #{msg}\n  queue: #{queue}\n  redis_pool: #{redis_pool}"
+  def call(worker_class, msg, queue)
+    puts "Sidekiq::Workflow::ServerMiddleware called with:\n  worker_class: #{worker_class}\n  msg: #{msg}\n  queue: #{queue}"
     yield
   end
 end
